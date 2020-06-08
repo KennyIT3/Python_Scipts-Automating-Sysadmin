fe01ddcc1dd26cf7f25b8acc6c430e5c52b14798
KennyIT3 <rellyrell92@gmail.com>
2020-06-08 18:20:23 +0000

    Update docker

--- Docker_Count.py
+++ Docker_Count.py
@@ -3,3 +3,16 @@ import re
 import os
 
 p = os.popen("docker images").read()
+docker=p
+
+columns = [list() for images in range(9)]
+for line in docker.split("\n"):
+    line = re.sub(" +", " ", line)
+    for images,dock in enumerate(line.split(" ")):
+        columns[images].append(dock)
+print("\n")
+#print("Removing header image:", columns[2].pop(0) , "\n")
+
+for images in columns[2]:
+     print("List of Images:", images)
+#print("List of Images:", columns[2], "\n")
