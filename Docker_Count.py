d1847ae53281eee44b9cbcd170b4471245760a6d
KennyIT3 <rellyrell92gmail.com>
2020-06-09 15:58:54 -0400

    Correct the change

--- Docker_Count.py
+++ Docker_Count.py
@@ -9,7 +9,7 @@ columns = [list() for images in range(9)]
 for line in docker.split("\n"):
     line = re.sub(" +", " ", line)
     for images,dock in enumerate(line.split(" ")):
-        columns[images].append(dock).sj
+        columns[images].append(dock)
 print("\n")
 #print("Removing header image:", columns[2].pop(0) , "\n")
 
