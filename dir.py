7096a1f660b4a0ba9f194af160d51d339a0c26b7
KennyIT3 <rellyrell92gmail.com>
2020-08-27 17:22:12 -0400

    Update

---
+++ dir.py
@@ -0,0 +1,33 @@
+#Python
+import os
+import sys
+
+
+rhel6= [ '/var' , '/root' , '/tmp' ]
+rhel6.sort()
+os.system("ls -al")
+
+for list in rhel6:
+    os.chdir(list)
+    s=os.getcwd()
+
+    print("\n")
+    print(s, "Here is the directory")
+    print("\n")
+    print("Here is disk usage for directory")
+    t=os.system("du -sh")
+    print("\n")
+
+    n=os.listdir(list)
+    print("Here are the files")
+    
+    for file in n:
+        print(file)
+
+
+def linux ():
+    print("\n")
+    print("File Systems Storage")
+    print("\n")
+    os.system("df -Th")
+linux()
