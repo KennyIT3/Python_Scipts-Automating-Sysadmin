9053810cf9318e0b0f0dd6e133c377aeba478e2b
KennyIT3 <rellyrell92@gmail.com>
2020-12-07 14:18:34 -0500

    Update Docker_Count

--- Scripts/Docker_Count.py
+++ Scripts/Docker_Count.py
@@ -7,14 +7,14 @@ p = os.popen("docker images").read()
 #p=subprocess.run(['docker' , 'images'])
 docker=p
 
-print(type((docker)))
+# print(type((docker)))
 
 columns = [list() for images in docker]
 for line in docker.split("\n"):
     line = re.sub(" +", " ", line)
     for images,dock in enumerate(line.split(" ")):
        columns[images].append(dock)
-print("\n")
+# print("\n")
 
-for images in columns[2]:
+for images in columns[2][1:]:
     print("List of Images:", images)
