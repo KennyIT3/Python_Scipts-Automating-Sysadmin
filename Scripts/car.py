c760cac94f2802d0bd912e6ccc641a025dd35e68
KennyIT3 <rellyrell92@gmail.com>
2020-12-11 00:22:50 -0500

    Update

--- Scripts/car.py
+++ Scripts/car.py
@@ -1,10 +1,11 @@
 try:
     class Car(object): 
-        def __init__(self, model, color, company, speed_limit):
+        def __init__(self, year, model, color, company, speed_limit):
             self.color = color
             self.company = company
             self.speed_limit = speed_limit
             self.model = model
+            self.year = year
 
         def start(self):
             print("started")
@@ -24,11 +25,11 @@ try:
             print("\nHere is the car details:")
             # print("\n")
 
-    audi = Car("A6", "red", "audi", 160)
-    print(f"\nWhat is the name of the car company: {audi.company} \nWhat is the model: {audi.model} \nWhat is the color: {audi.color} \nWhat is the car speed: {audi.speed_limit}")
-    print(f"The car has now:"), audi.start()
+    jaguar = Car("2020", "F-Type", "Black", "Jaguar", 260)
+    print(f" \nWhat is the car year: {jaguar.year}\nWhat is the name of the car company: {jaguar.company} \nWhat is the model: {jaguar.model} \nWhat is the color: {jaguar.color} \nWhat is the car speed: {jaguar.speed_limit}")
+    print(f"The car has now:"), jaguar.start()
     # audi.accelarate()
-    
+
 except(ValueError,RuntimeError):
     "The car model is incorrect"
     pass
